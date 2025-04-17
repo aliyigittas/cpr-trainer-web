2d6c3901282cde15f8e2f8edf83270fa450a527a
SevvalCetin <scetin@stu.khas.edu.tr>
2025-04-17 19:50:48 +0300

    Google Cloud SQL Connection control

--- cpr-trainer-backend/src/main/java/com/lpsoft/cpr_trainer_backend/CprTrainerBackendApplication.java
+++ cpr-trainer-backend/src/main/java/com/lpsoft/cpr_trainer_backend/CprTrainerBackendApplication.java
@@ -1,7 +1,12 @@
 package com.lpsoft.cpr_trainer_backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class CprTrainerBackendApplication {
@@ -10,4 +15,15 @@ public class CprTrainerBackendApplication {
 		SpringApplication.run(CprTrainerBackendApplication.class, args);
 	}
 
+	    // Uygulama başladığında bağlantı testi yapacak
+	@Bean
+    CommandLineRunner testConnection(DataSource dataSource) {
+        return args -> {
+            try (Connection connection = dataSource.getConnection()) {
+                System.out.println("✅ Veritabanı bağlantısı başarılı!");
+            } catch (Exception e) {
+                System.err.println("❌ Veritabanı bağlantısı başarısız: " + e.getMessage());
+            }
+        };
+    }
 }
