1340108c860c2cc2d8cdd1c8c91fc3216f7c6fa1
Ali Yiğit Taş <aliyigittas@stu.khas.edu.tr>
2025-04-17 20:21:30 +0300

    Add JDBC dependencies and implement performance data endpoint

--- cpr-trainer-backend/src/main/java/com/lpsoft/cpr_trainer_backend/CprTrainerBackendApplication.java
+++ cpr-trainer-backend/src/main/java/com/lpsoft/cpr_trainer_backend/CprTrainerBackendApplication.java
@@ -1,12 +1,9 @@
 package com.lpsoft.cpr_trainer_backend;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 public class CprTrainerBackendApplication {
@@ -16,14 +13,36 @@ public class CprTrainerBackendApplication {
 	}
 
 	    // Uygulama başladığında bağlantı testi yapacak
-	@Bean
-    CommandLineRunner testConnection(DataSource dataSource) {
-        return args -> {
-            try (Connection connection = dataSource.getConnection()) {
-                System.out.println("✅ Veritabanı bağlantısı başarılı!");
-            } catch (Exception e) {
-                System.err.println("❌ Veritabanı bağlantısı başarısız: " + e.getMessage());
-            }
-        };
+	// @Bean
+    // CommandLineRunner testConnection(DataSource dataSource) {
+    //     return args -> {
+    //         try (Connection connection = dataSource.getConnection()) {
+    //             System.out.println("✅ Veritabanı bağlantısı başarılı!");
+    //             //insert to users table
+    //             String sql = """
+    //                     INSERT INTO `cpr`.`users` (firstname, surname, username, email, password, khasID) VALUES (?, ?, ?, ?, ?, ?)
+    //             """;
+    //             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    //                 preparedStatement.setString(1, "John");
+    //                 preparedStatement.setString(2, "Doe");
+    //                 preparedStatement.setString(3, "johndoe");
+    //                 preparedStatement.setString(4, "mail@mail.com");
+    //                 preparedStatement.setString(5, "password");
+    //                 preparedStatement.setString(6, "123456789");
+    //                 preparedStatement.executeUpdate();
+    //                 System.out.println("✅ Kullanıcı başarıyla eklendi!");
+    //             } catch (SQLException e) {
+    //                 System.err.println("❌ Kullanıcı eklenirken hata oluştu: " + e.getMessage());
+    //             }
+    //         } catch (Exception e) {
+    //             System.err.println("❌ Veritabanı bağlantısı başarısız: " + e.getMessage());
+    //         }
+    //     };
+    // }
+
+    @PostMapping("/performance")
+    public String addPerformanceData(@RequestBody String userData) {
+        System.out.println("Received performance data: " + userData);
+        return "Performance data added successfully!";
     }
 }
