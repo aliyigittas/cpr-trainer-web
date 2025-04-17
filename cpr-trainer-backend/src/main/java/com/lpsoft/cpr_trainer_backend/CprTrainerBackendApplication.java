a786303cb91bf3bb89e0d080c78327baeb2d9883
SevvalCetin <scetin@stu.khas.edu.tr>
2025-04-17 20:43:09 +0300

    Unity and Spring Boot connection is completed.

--- cpr-trainer-backend/src/main/java/com/lpsoft/cpr_trainer_backend/CprTrainerBackendApplication.java
+++ cpr-trainer-backend/src/main/java/com/lpsoft/cpr_trainer_backend/CprTrainerBackendApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class CprTrainerBackendApplication {
 
 	public static void main(String[] args) {
