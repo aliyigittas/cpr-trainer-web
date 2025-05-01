da5ae176efc3f1bcaa8ed6997b440ae002ceacf6
SevvalCetin <scetin@stu.khas.edu.tr>
2025-05-01 10:09:51 +0300

    AuthController update for updating instructor note.

--- cpr-trainer-backend/src/main/java/com/lpsoft/cpr_trainer_backend/AuthController.java
+++ cpr-trainer-backend/src/main/java/com/lpsoft/cpr_trainer_backend/AuthController.java
@@ -206,7 +206,12 @@ public class AuthController {
                 if (!user.getRole().equals("instructor")) {
                     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Only instructors can add notes.");
                 }
-                databaseAdapter.saveInstructorNote(Integer.parseInt(performanceid), notetype, note);
+                if(performanceNotesRepository.findByPerformanceidAndType(Integer.parseInt(performanceid), notetype).isEmpty()){ //if the is no instructor note added 
+                    databaseAdapter.saveInstructorNote(Integer.parseInt(performanceid), notetype, note);
+                }
+                else{
+                    databaseAdapter.updateInstructorNote(Integer.parseInt(performanceid), notetype, note);
+                }
                 //TODO: create dump to database
             }
             
